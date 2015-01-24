7b0966cd462a80767ffad1ea0abf52891f5f029a
SjRNMzU <james@jmpe.me>
2015-01-24 01:00:37 +0000

    tidy code, add data statistics

--- main.cpp
+++ main.cpp
@@ -30,7 +30,6 @@ unsigned char decode(unsigned char start, unsigned char next)
 //data is encoded 2 bytes per pixel as teh difference between the R->G and G->B values
 void embedImageData(Magick::PixelPacket *pixels, size_t width, size_t height, const unsigned char *data, size_t data_len, size_t imgColDepth)
 {
-
     string quantumColDepth = MagickQuantumDepth;
     size_t QDepth;
     if(quantumColDepth.compare("Q8") == 0){
@@ -44,10 +43,7 @@ void embedImageData(Magick::PixelPacket *pixels, size_t width, size_t height, co
         exit(EXIT_FAILURE);
     }
 
-    cout << MagickQuantumDepth << endl;
-    cout << "imgColDepth: " << imgColDepth << endl;
     uint32_t pow_diff = QDepth - imgColDepth;
-    cout << "Pow Diff: " << pow_diff << endl;
 
     //prefix data with length
     uint32_t len;
@@ -60,19 +56,25 @@ void embedImageData(Magick::PixelPacket *pixels, size_t width, size_t height, co
     auto encodeQuantum = [&] (Magick::Quantum a, unsigned char data) {
         return Magick::Quantum((a & 0xFFFFFFF0) | encode(a & 0x0F, 0x0F & data));
     };
-
+#ifdef DEBUG
+    cout << MagickQuantumDepth << endl;
+    cout << "imgColDepth: " << imgColDepth << endl;
+    cout << "Pow Diff: " << pow_diff << endl;
     cout << "Data len: " << data_len << endl;
     cout << "Total len: " << len << endl;
     cout << "Length as bytes: " << hexPrint(buff, 4) << endl;
     cout << "data: " << hexPrint(buff, len) << endl;
+#endif
 
     uint32_t bytes = 0;
     for(size_t i=0; i<height; ++i){
         for(size_t j=0; j<width; ++j){
             if(bytes >= len)
-                break;
+                return;
 
+#ifdef DEBUG
             cout << endl << "Pixel: (" << j << "," << i << ")" << endl;
+#endif
             Magick::PixelPacket *pix_packt = pixels+((width*i) + j);
 
             pix_packt->red = pix_packt->red >> pow_diff;
@@ -86,17 +88,15 @@ void embedImageData(Magick::PixelPacket *pixels, size_t width, size_t height, co
 
             //set green quatum -> green quatum last nibble is encoded
             //encoded nibble is produced by encoding first nibble of data and last nibble or red quantum
-            // pix_packt->green = ((pix_packt->green & 0xFFFFFFF0) | (encodeQuantum(pix_packt->red, (buff[bytes] & 0xF0) >> 4) & 0x0F));
-            // pix_packt->blue = ((pix_packt->blue & 0xFFFFFFF0) | (encodeQuantum(pix_packt->green, (buff[bytes] & 0x0F)) & 0x0F));
+            green = (green & 0xFFFFF0) | encodeQuantum( red & 0x0F , ( buff[bytes] >> 4 ) & 0x0F );
+            blue = (blue & 0xFFFFF0) | encodeQuantum( green & 0x0F , ( buff[bytes] & 0x0F) );
 
-            green =     (green & 0xFFFFF0) | encodeQuantum( red & 0x0F , ( buff[bytes] >> 4 ) & 0x0F );
-            blue =       (blue & 0xFFFFF0) | encodeQuantum( green & 0x0F , ( buff[bytes] & 0x0F) );
-
-            // printf("Red: 0x%.02X, Green: 0x%.02X, Blue: 0x%.02X\n", red & 0xFF, green & 0xFF, blue & 0xFF);
+#ifdef DEBUG
             printf("Red: 0x%.4X, Green: 0x%.4X, Blue: 0x%.4X\n", pix_packt->red, pix_packt->green, pix_packt->blue);
             printf("\tInput: 0x%.2X, Data: 0x%.2X, Out: 0x%.02X\n", red & 0x0F, (buff[bytes] & 0xF0) >> 4 , green & 0xFF);
             printf("\tInput: 0x%.2X, Data: 0x%.2X, Out: 0x%.02X\n", green & 0x0F, buff[bytes] & 0x0F, blue & 0xFF);
             printf("Red: 0x%.02X, Green: 0x%.02X, Blue: 0x%.02X\n", red, green, blue);
+#endif
 
 
             //Set pixel color
@@ -108,13 +108,11 @@ void embedImageData(Magick::PixelPacket *pixels, size_t width, size_t height, co
             *pix_packt = pixel_color;
             bytes++;
         }
-        if(bytes >= len)
-            break;
     }
     return;
 }
 
-
+#ifdef DEBUG
 void dumpPixels(Magick::PixelPacket *pixels, size_t width, size_t height, size_t N)
 {
     size_t it = 0;
@@ -128,6 +126,7 @@ void dumpPixels(Magick::PixelPacket *pixels, size_t width, size_t height, size_t
         }
     }
 }
+#endif
 
 
 unsigned char *pixelCacheToBytes(Magick::PixelPacket *pixels, size_t width, size_t height, size_t imgColDepth)
@@ -145,10 +144,12 @@ unsigned char *pixelCacheToBytes(Magick::PixelPacket *pixels, size_t width, size
         exit(EXIT_FAILURE);
     }
 
+    uint32_t pow_diff = QDepth - imgColDepth;
+#ifdef DEBUG
     cout << MagickQuantumDepth << endl;
     cout << "imgColDepth: " << imgColDepth << endl;
-    uint32_t pow_diff = QDepth - imgColDepth;
     cout << "Pow Diff: " << pow_diff << endl;
+#endif
 
     unsigned char *data = (unsigned char *)malloc(width*height);
     memset(data, 0, width*height);
@@ -158,23 +159,15 @@ unsigned char *pixelCacheToBytes(Magick::PixelPacket *pixels, size_t width, size
         return decode((a & 0x0000000F) , (b & 0x0000000F));
     };
 
-    cout << "Recovered data:\n";
+    // cout << "Recovered data:\n";
     for(size_t i=0; i<height; ++i){
         for(size_t j=0; j<width; ++j){
             Magick::PixelPacket *pix_packt = pixels+((width*i) + j);
             unsigned char rg = 0x0F & decodeQuantum(pix_packt->red >> pow_diff, pix_packt->green >> pow_diff);
             unsigned char gb = 0x0F & decodeQuantum(pix_packt->green >> pow_diff, pix_packt->blue >> pow_diff);
             data[it] = (rg << 4) | gb;
-            // cout << hexPrint(&rg, 1) << " ";
-            // cout << hexPrint(&gb, 1) << " ";
-            // cout << hexPrint(&data[it], 1) << "\n";
             it++;
-
-            // if(it > 10)
-            //     break;
         }
-        // if(it > 10)
-        //     break;
     }
     //Decoded PixelArray
     return data;
@@ -183,14 +176,16 @@ unsigned char *pixelCacheToBytes(Magick::PixelPacket *pixels, size_t width, size
 string uncoverImageData(Magick::PixelPacket *pixels, uint32_t width, uint32_t height, size_t imgColDepth)
 {
     unsigned char *data = pixelCacheToBytes(pixels, width, height, imgColDepth);
+#ifdef DEBUG
     cout << "First 4 bytes of data: " << hexPrint(data, 4) << endl;
+#endif
 
     uint32_t len;
     //get len
     memcpy(&len, &data[0], 4);
 
     cout << "Encoded data size: " << len << endl;
-    assert(len < 100);
+    assert(len < width * height);
 
     //-4 for size of int32_t + 1 for null pointer added on by snprintf
     char buff[len - 4 + 1];
@@ -217,7 +212,7 @@ inline string hexPrint(const unsigned char *data, const size_t len)
 }
 
 
-
+#ifdef DEBUG
 void incise(char *surface, size_t surface_len, char *ink, size_t ink_len)
 {
     assert(surface_len > ink_len + 1);
@@ -235,34 +230,20 @@ void deincise(char *surface, size_t surface_len, char *ink, size_t ink_len)
     }
     return;
 } 
+#endif
 
 int main(int argc, char *argv[])
 {
-    char blob[8] = {"abcdefg"};
-    char data[6] = {"Hello"};
-    char encoded_blob[8];
-    char decoded[8]; 
-
-    //copy blob into encoded blob, set decoded buff to 0
-    memcpy(encoded_blob, blob, 8);
-    memset(decoded, 0, 8);
-    
-    incise(encoded_blob, 8, data, 6);
-    deincise(encoded_blob, 8, decoded, 6);
-
-    cout << "blob: " << hexPrint(blob, 8) << endl;
-    cout << "data: " << hexPrint(data, 6) << endl;
-    cout << "encoded_blob: "<< hexPrint(encoded_blob, 8) << endl;
-    cout << "data: " << data << endl;
-    cout << "decoded: " << decoded << endl;
-
-    cout << endl << ">>>>\t1m4g33nc0d3r\t<<<<" << endl << endl;
-
-    string data_str("Hello");
-    cout << "Data to encode: " << data_str << endl;
-    cout << "Data to encode: " << hexPrint(data_str.c_str(), data_str.size()) << endl;
+    if(argc < 3){
+        cerr << "Useage: 1m4g33nc0d3r -i image.ext e/d -o output" << endl;
+        exit(EXIT_FAILURE);
+    }
 
     Magick::InitializeMagick(*argv);
+
+    //print init screen
+    cout << endl << ">>>>\t1m4g33nc0d3r\t<<<<" << endl << endl;
+
 
     string imagePath = argv[1];
     Magick::Image *img = new Magick::Image(imagePath);
@@ -270,50 +251,57 @@ int main(int argc, char *argv[])
 
     size_t width = img->columns();
     size_t height = img->rows();
-    size_t bpp = 2;
-    size_t modDepth = img->modulusDepth();
     size_t depth = img->depth();
 
-    cout << "Bits Per Pixel: " << bpp << endl;
-    cout << "Image mod depth: " << modDepth << endl;
-    cout << "Image depth: " << depth << endl;
-    cout << "Image size: " << width << "x" << height << endl;
-
-
-    // Magick::Pixels pixel_cache(*img);
-    // Magick::PixelPacket *pixels;
-    // pixels = pixel_cache.get(0, 0, width, height);
+    cout << "Input Image: " << argv[1] << endl;
+    cout << "\tImage size: " << width << "x" << height << endl;
+    cout << "\tImage depth: " << depth << endl;
+    cout << endl;
 
     Magick::PixelPacket *pixels = img->getPixels(0, 0, width, height);
-
-    // uint32_t bytes = 0;
-    if((data_str.size() + 1) > (width*height) ){
-        cerr << "Error not enough space to encode data in image" << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    unsigned char data_buff[data_str.size()];
-    memcpy(data_buff, data_str.c_str(), data_str.size());
-
-    // return 0;
 
     char op = *argv[2];
     switch(op){
         case 'e':{
-            dumpPixels(pixels, width, height, 12);
-            embedImageData(pixels, width, height, data_buff, data_str.size(), depth);
+            //Print out data encoding
+            string data("Hello World!");
+            for(int i=0; i < 13; ++i){
+                data += data;
+            }
+            cout << "Image file size: " << img->fileSize() << "B" << endl;
+            cout << "Data Size: " << data.size() << "B" << endl;
+            cout << "Compression ratio: " << static_cast<float>(data.size())/static_cast<float>(img->fileSize()) << endl;
+
+            // cout << "Data: " << data << endl;
+            // cout << "hex(Data): " << hexPrint(data.c_str(), data.size()) << endl;
+
+            if((data.size() + 1) > (width*height) ){
+                cerr << "Error not enough space to encode data in image" << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            //copy data into binary data_buff
+            unsigned char data_buff[data.size()];
+            memcpy(data_buff, data.c_str(), data.size());
+
+            #ifdef DEBUG
+            dumpPixels(pixels, width, height, data.size() + 4);
+            #endif
+            embedImageData(pixels, width, height, data_buff, data.size(), depth);
             img->syncPixels();
+            cout << "Saving to: " << argv[3] << endl;
             img->write(argv[3]);
-            dumpPixels(pixels, width, height, 12);
-            // uint32_t kbytes = bytes / 1024;
-            // bytes = bytes % 1024;
-            // cout << "Total Bytes of space used: " << kbytes << "KB " << bytes << "B" << endl;
+            #ifdef DEBUG
+            dumpPixels(pixels, width, height, data.size() + 4);
+            #endif
             break;
             }
         case 'd':{
-            dumpPixels(pixels, width, height, 12);
+            #ifdef DEBUG
+            dumpPixels(pixels, width, height, data.size() + 4);
+            #endif
             string decodedData = uncoverImageData(pixels, width, height, depth);
-            cout << "Decoded data: " << decodedData.c_str() << endl;
+            cout << "Recovered data: " << decodedData << endl;
             break;
             }
         default:
